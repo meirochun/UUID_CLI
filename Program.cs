11bb64bca9a14f505d1c1ea4f21530082321ac99
meirochun <meirochun@gmail.com>
2025-05-05 22:59:10 -0300

    add cli project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.CommandLine;
+using TextCopy;
+
+var rootCmd = new RootCommand("guid");
+
+rootCmd.SetHandler(() =>
+{
+    var guid = GenerateAndCopyGuid();
+    DisplayGuid(guid);
+});
+
+return await rootCmd.InvokeAsync(args);
+
+static Guid GenerateAndCopyGuid()
+{
+    var guid = Guid.NewGuid();
+    ClipboardService.SetText(guid.ToString());
+    return guid;
+}
+
+static void DisplayGuid(Guid guid)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("UUID Copied! ");
+    Console.ResetColor();
+    Console.Write(guid.ToString());
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# GUID CLI
+
+A veeeeeeeeeery simple cli command to generate random uuid's.
